be960d243bb60a10ba6be06126c708474adb2791
Jason Ray Mahawan <jasonraymahawan123@gmail.com>
2025-03-23 15:33:57 +0800

    Add user creation functionality

---
+++ src/routes/user.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createUser } from '../controllers/userController';
+
+const router = express.Router();
+
+// Route for creating a user
+router.post('/users', createUser);
+
+export default router;
